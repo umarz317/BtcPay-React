0121895faae9b178c5d4d7c5959c4dc3cb928345
umarz317 <umar.317@gmail.com>
2023-02-20 22:56:10 +0500

    update

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -4,7 +4,7 @@ const Pay = () => {
             <h1 className="font">Pay in Bitcoin!</h1>
             <h1 className="font green">LIMITED TIME OFFER! <del>$249.99</del> $1</h1>
             <img src="https://rapidpassportco.com/assets/images/image19.png?v=e314f384" width="50px" />
-            <form style={{ marginTop: '10px' }} method="POST" action="https://testnet.demo.btcpayserver.org/apps/dLX6vBctkzp15KooezCBpCUNcQ5/pos">
+            <form style={{ marginTop: '10px' }} method="POST" action="https://mainnet.demo.btcpayserver.org/apps/41mrMhAq7AiNmGTpmBEXcNq7zwmK/pos">
                 <input type="hidden" name="email" value="customer@example.com" />
                 <input type="hidden" name="orderId" value="CustomOrderId" />
                 <input type="hidden" name="notificationUrl" value="https://example.com/callbacks" />
