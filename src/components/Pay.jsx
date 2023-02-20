f974ec318c624dc486a2811d7bcd403bb52bf4a3
umarz317 <umar.317@gmail.com>
2023-02-20 22:31:51 +0500

    update

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -2,7 +2,7 @@ const Pay = () => {
     return (
         <div>
             <h1 className="font">Pay in Bitcoin!</h1>
-            <h1 className="font green">LIMITED TIME OFFER! <del>$249.99</del> $149.99</h1>
+            <h1 className="font green">LIMITED TIME OFFER! <del>$249.99</del> $1</h1>
             <img src="https://rapidpassportco.com/assets/images/image19.png?v=e314f384" width="50px" />
             <form style={{ marginTop: '10px' }} method="POST" action="https://testnet.demo.btcpayserver.org/apps/dLX6vBctkzp15KooezCBpCUNcQ5/pos">
                 <input type="hidden" name="email" value="customer@example.com" />
