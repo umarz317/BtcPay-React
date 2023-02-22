dcfc4f38396c78452b0ddac1124576103eb7b969
umarz317 <umar.317@gmail.com>
2023-02-22 19:26:13 +0500

    updated complete page

--- src/components/CallBack.jsx
+++ src/components/CallBack.jsx
@@ -1,7 +1,11 @@
+import tick from "../assets/images/greentick.png"
 const CallBack = () => {
     return (
-        <div>
+        <div className="pay">
+            <div style={{textAlign:"center"}}>
+            <img width="50px" src={tick}/>
             <h1 className="font">Thank you for the payment!</h1>
+            </div>
         </div>
     );
 }
